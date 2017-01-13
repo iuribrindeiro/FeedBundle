351934b1380172818e9b902202180aa36d268d8a
Iuri Brindeiro <iuribrindeiro@gmail.com>
2017-01-13 16:02:56 -0300

    resolvendo erro de chamada de metodo FeedRepository

--- Controller/FeedController.php
+++ Controller/FeedController.php
@@ -301,7 +301,7 @@ class FeedController extends FormController
 
         $security = $this->get('mautic.security');
         /** @var Feed $entity */
-        $entity   = $model->getRegetFeedOrderByDateSent($objectId);
+        $entity   = $model->getRepository()->getFeedOrderByDateSent($objectId);
 
 
         /** @var Article $article */
