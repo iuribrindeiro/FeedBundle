096ad81919c68c16f06e2a066d464032af2c19ba
Iuri <iuribrindeiro@gmail.com>
2017-01-29 20:00:35 -0300

    ajustando layout de email

--- Views/Feed/email-feed.html.php
+++ Views/Feed/email-feed.html.php
@@ -1,14 +1,16 @@
 <html>
 
 <?php foreach($feeds as $feed): ?>
-    <div>
-        <?php
-            $conteudo = $feed->getDescription();
-            $stringLength = strlen($conteudo);
-            $conteudo = substr($conteudo, 0, $stringLength/2) .  '<a title="...leia mais" href="' . $feed->getLink() . '">...leia mais</a>';
-        ?>
-        <h2><a href="<?= $feed->getPublicId(); ?>" title="...leia mais"><?= $feed->getTitle() ?></a></h2>
-        <p><?= $conteudo ?></p>
+    <div style="background-color: #e8e8e8">
+        <div style="background-color: white; margin-left: 12%; margin-right: 12%">
+            <?php
+                $conteudo = $feed->getDescription();
+                $stringLength = strlen($conteudo);
+                $conteudo = substr($conteudo, 0, $stringLength/2) .  '...<a title="...leia mais" href="' . $feed->getLink() . '">[leia mais]</a>';
+            ?>
+            <h2 style="padding-left: 10%; padding-right: 10%"><a href="<?= $feed->getPublicId(); ?>" title="...leia mais"><?= strtoupper($feed->getTitle()); ?></a></h2>
+            <p style="text-align: left; margin-top: 2%; margin-right: 4%; margin-left: 4%; margin-bottom: 2%;"><?= $conteudo ?></p>
+        </div>
     </div>
 <?php endforeach; ?>
 
